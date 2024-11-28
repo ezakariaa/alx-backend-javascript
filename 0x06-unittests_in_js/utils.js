5ee772e3ae34c72c8454cadf4f3fe184bdcf5845
ezakariaa <zakariaorche@gmail.com>
2024-11-28 14:43:15 +0000

    0x06. Unittests in JS

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,10 @@
+const Utils = {
+  calculateNumber: (type, a, b) => {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    throw new Error('Invalid type');
+  }
+};
+
+module.exports = Utils;
